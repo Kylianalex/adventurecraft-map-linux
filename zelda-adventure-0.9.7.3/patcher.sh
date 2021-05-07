1db6a5a8225cdb7195c00b3c1625bc757d3ea5e2
Kylianalex <66625058+Kylianalex@users.noreply.github.com>
2021-05-07 13:19:56 +0200

    Create patcher.sh

---
+++ zelda-adventure-0.9.7.3/patcher.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+###########################
+# Please make a backup of the map before using this program
+# made by Kylianalex
+###########################
+# need to be executed in ~/.config/adventurecraft-launcher/GameFiles/mc173/maps/<map name>/
+# if you have the snap package ~/snap/adventurecraft-launcher/x3/.config/adventurecraft-launcher/GameFiles/mc173/maps/<map name>/
+###########################
+
+mv ./thumbnail.PNG ./thumbnail.png
+mv ./boss/slime.png ./boss/Slime.png
+mv ./boss/Worm_head.png ./boss/worm_head.png
+mv ./boss/Worm_tail_end.png ./boss/worm_tail_end.png
+mv ./fire/bat.png ./fire/Bat.png
+mv ./fire/creeper.png ./fire/Creeper.png
+mv ./fire/skeleton.png ./fire/Skeleton.png
+mv ./fire/skeletonBoss.png ./fire/SkeletonBoss.png
+mv ./fire/skeletonSword.png ./fire/SkeletonSword.png
+mv ./fire/slime.png ./fire/Slime.png
+mv ./fire/spider.png ./fire/Spider.png
+mv ./ice/bat.png ./ice/Bat.png
+mv ./ice/creeper.png ./ice/Creeper.png
+mv ./ice/skeleton.png ./ice/Skeleton.png
+mv ./ice/skeletonSword.png ./ice/SkeletonSword.png
+mv ./ice/slime.png ./ice/Slime.png
+mv ./ice/spider.png ./ice/Spider.png
+mv ./tenebre/creeper.png ./tenebre/Creeper.png
+mv ./tenebre/skeleton.png ./tenebre/Skeleton.png
+mv ./tenebre/skeletonBoss.png ./tenebre/SkeletonBoss.png
+mv ./tenebre/skeletonSword.png ./tenebre/SkeletonSword.png
+mv ./tenebre/slime.png ./tenebre/Slime.png
+mv ./tenebre/spider.png ./tenebre/Spider.png
+mv ./tenebre/zombie.png ./tenebre/Zombie.png
+mv ./tenebre/zombieBoss.png ./tenebre/ZombieBoss.png
+mv ./gui/decompte.PNG ./gui/decompte.png
+mv ./gui/magie3.png ./gui/magie3.PNG
+mv ./models/mob/eye.PNG ./models/mob/eye.png
+mv ./models/mob/sluganimated.png ./models/mob/SlugAnimated.png
+mv ./models/mob/SlugEye.PNG ./models/mob/SlugEye.png
+mv ./models/mob/slugnoflames.png ./models/mob/SlugNoFlames.png
+mv ./npc/invisible.png ./npc/Invisible.png
+mv ./overlays/Ocarina_melody.png ./overlays/ocarina_melody.png
+mv ./sprites/MenuArrows.PNG ./sprites/MenuArrows.png
+cp ./water/slime.png ./water/Slime.png #can be a problem if you don't have a regular partition
