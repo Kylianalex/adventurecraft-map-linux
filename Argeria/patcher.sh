bd17ce50c76051e1996b06b0600fb8d515ac8c82
Kylianalex <66625058+Kylianalex@users.noreply.github.com>
2021-05-07 15:08:15 +0200

    Create patcher.sh

---
+++ Argeria/patcher.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+###########################
+# Please make a backup of the map before using this program
+# made by Kylianalex
+###########################
+# need to be executed in ~/.config/adventurecraft-launcher/GameFiles/mc173/maps/<map name>/
+# if you have the snap package ~/snap/adventurecraft-launcher/x3/.config/adventurecraft-launcher/GameFiles/mc173/maps/<map name>/
+###########################
+
+mv ./Overlays ./overlays
+mv ./Npc ./npc
+mv ./npc/guard.png ./npc/Guard.png
