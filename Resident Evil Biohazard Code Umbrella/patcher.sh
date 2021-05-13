74dce2e94d8b3b1926e85cea79f910463a22aa94
Kylianalex <66625058+Kylianalex@users.noreply.github.com>
2021-05-13 13:00:49 +0200

    Create patcher.sh

---
+++ Resident Evil Biohazard Code Umbrella/patcher.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+###########################
+# Please make a backup of the map before using this program
+# made by Kylianalex
+###########################
+# need to be executed in ~/.config/adventurecraft-launcher/GameFiles/mc173/maps/<map name>/
+# if you have the snap package ~/snap/adventurecraft-launcher/x3/.config/adventurecraft-launcher/GameFiles/mc173/maps/<map name>/
+###########################
+
+mv ./Overlays ./overlays
+mv ./overlays/Resident\ Evil.png ./overlays/Resident\ evil.png
+mv ./overlays/Lettre.png ./overlays/lettre.png
+mv ./overlays/Lettre2.png ./overlays/lettre2.png
